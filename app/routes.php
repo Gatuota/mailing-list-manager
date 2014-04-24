e663b5f9ee63082b3f62d10588b6de54bd37e015
Ryan Durham <rydurham@gmail.com>
2014-04-23 20:00:10 -0700

    Prepare routes file

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', function()
+// Set Home Route
+ Route::get('/', array('as' => 'home', function()
 {
-	return View::make('hello');
-});
+    return View::make('hello');
+}));
